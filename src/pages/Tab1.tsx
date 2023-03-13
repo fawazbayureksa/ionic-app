cd58abecd252aa31e0e2b2fcfea1c6b37895e42c
Fawwaz Bayureksa <fawwaz.bayureksa@singa.id>
2023-03-13 09:12:38 +0700

    first commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonCol, IonContent, IonHeader, IonIcon, IonPage, IonRow, IonText, IonTitle, IonToolbar, IonVirtualScroll } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { chatbubble, chatbubbleOutline, heartOutline } from 'ionicons/icons';
+
 
 const Tab1: React.FC = () => {
+
+  const data = [{ name: "revelvetcake" }, { name: "wahyu" }, { name: "iqbal" }, { name: "Agung" }, { name: "Anto" }]
+
+  function truncate(str: string) {
+    return str.length > 5 ? str.substring(0, 5) + "..." : str;
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonRow class="ion-justify-content-between">
+            <IonCol size="10">
+              <IonTitle>Instagram</IonTitle>
+            </IonCol>
+            <IonCol>
+              <IonIcon aria-hidden="true" size='large' icon={heartOutline} />
+              <IonIcon aria-hidden="true" size='large' icon={chatbubbleOutline} />
+            </IonCol>
+          </IonRow>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonRow class="ion-align-items-center ion-justify-content-start content-story" >
+          {data.map((a, id) => (
+            <IonAvatar class='text-center mr-2' key={id}>
+              <img alt={a.name} className='img-outline' src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+              <IonText>{truncate(a.name)}</IonText>
+            </IonAvatar>
+          ))}
+        </IonRow>
+        {/* <ExploreContainer name="Tab 1 page" /> */}
       </IonContent>
     </IonPage>
   );
